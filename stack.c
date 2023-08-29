91753650b8c518f41638845c767ce0388d85e218
Emmanuel <oladepogboyega@gmail.com>
2023-08-29 10:30:17 -0700

    Our Monty Project

--- stack.c
+++ stack.c
@@ -1,17 +1,64 @@
 #include "monty.h"
+#include <string.h>
+
+void free_stack(stack_t **stack);
+int init_stack(stack_t **stack);
+int check_mode(stack_t *stack);
+	
+/**
+* free_stack - Frees a stack_t stack.
+* @stack: A pointer to the top (stack) or
+*         bottom (queue) of a stack_t.
+*/
+void free_stack(stack_t **stack)
+{
+stack_t *tmp = *stack;
+
+while (*stack)
+{
+tmp = (*stack)->next;
+free(*stack);
+*stack = tmp;
+}
+}
 
 /**
- * f_stack - Initializes a value in the bus for unspecified purpose.
- * @head: Unused parameter (Pointer to the stack's head).
- * @counter: Unused parameter (Line number being executed).
- * Return: No return value.
- */
-void f_stack(stack_t **head, unsigned int counter)
+* init_stack - Initializes a stack_t stack with beginning
+* stack and ending queue nodes.
+* @stack: A pointer to an unitialized stack_t stack.
+* Return: If an error occurs - EXIT_FAILURE.
+* Otherwise - EXIT_SUCCESS.
+*/
+int init_stack(stack_t **stack)
 {
-    /* These parameters are currently not used */
-	(void)head;
-	(void)counter;
+stack_t *s;
 
-    /* Set an unspecified value in the bus for an unclear purpose */
-	bus.lifi = 0;
+s = malloc(sizeof(stack_t));
+if (s == NULL)
+return (malloc_error());
+
+s->n = STACK;
+s->prev = NULL;
+s->next = NULL;
+
+*stack = s;
+
+return (EXIT_SUCCESS);
+}
+
+/**
+* check_mode - Checks if a stack_t linked list is in stack or queue mode.
+* @stack: A pointer to the top (stack) or bottom (queue)
+* of a stack_t linked list.
+* Return: If the stack_t is in stack mode - STACK (0).
+* If the stack_t is in queue mode - QUEUE (1).
+* Otherwise - 2.
+*/
+int check_mode(stack_t *stack)
+{
+if (stack->n == STACK)
+return (STACK);
+else if (stack->n == QUEUE)
+return (QUEUE);
+return (2);
 }
